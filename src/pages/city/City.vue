9aad55699c82d15de2c71bffc582b00c55a97ada
wujiahui <806443173@qq.com>
2021-12-29 00:55:58 +0800

    【add】跳转城市路由

---
+++ src/pages/city/City.vue
@@ -0,0 +1,15 @@
+<template>
+  <city-header></city-header>
+</template>
+<script>
+import CityHeader from './component/Header'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
